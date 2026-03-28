6740049232bd5e5f64b8969274ef9e3cbe5985ac
Tianhao Hu <55843000+underwoodnoble@users.noreply.github.com>
2026-03-28 10:01:58 +0800

    提供不同的主题

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -2,3 +2,14 @@ vim.api.nvim_create_user_command("Rfinder", function()
   local path = vim.api.nvim_buf_get_name(0)
   os.execute("open -R " .. path)
 end, {})
+
+vim.api.nvim_create_user_command("Light", function()
+  vim.cmd("colorscheme default | set background=light")
+end, {})
+
+vim.api.nvim_create_user_command("Dark", function()
+  vim.cmd("colorscheme oxocarbon | set background=dark")
+end, {})
+
+-- Set default colorscheme to light
+vim.cmd("Light")
---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        cuda = { "clang-format" },
+      },
+    },
+  },
+}
